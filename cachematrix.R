88314d0c2a96b0ad9e23b18f841544e7f47dad6e
phc07 <phc07.cheng@gmail.com>
2017-03-02 18:26:15 +0800

    Completed Assignment
    Complete the assignment and upload to GitHub

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,6 @@
 ## Put comments here that give an overall description of what your
 ## functions do
-
+## Submit to GitHub as assignment
 ## Write a short comment describing this function
 ##makeCacheMatrix function creates a special "matrix" object that can cache its inverse
 makeCacheMatrix <- function(x = matrix()) {
