e563a662acc9c6b7d544e59d74e985027aa65b4d
phc07 <phc07.cheng@gmail.com>
2017-03-02 10:28:32 +0800

    Starting
    Start to work on the assignment

--- cachematrix.R
+++ cachematrix.R
@@ -2,9 +2,18 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+##makeCacheMatrix function creates a special "matrix" object that can cache its inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        invmatrix <- function(solve) m <<- invmatrix
+        list(set = set, 
+             get = get,
+             invmatrix = invmatrix)
 }
 
 
